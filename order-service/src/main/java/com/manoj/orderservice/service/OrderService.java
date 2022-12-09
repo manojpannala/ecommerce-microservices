64a9d92497a978046ee3e660bedc025ac58255f5
Manoj Reddy Pannala <manojpannala@outlook.com>
2022-12-09 17:11:02 +0200

    add @LoadBalanced to WebClientConfig

--- order-service/src/main/java/com/manoj/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/manoj/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -42,7 +42,8 @@ public class OrderService {
          TODO Call inventory service and place order
          if product is in stock
           */
-        InventoryResponse[] inventoryResponseArray = webClient.get()
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build()
+                .get()
                 .uri("http://inventory-service/api/inventory/",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
