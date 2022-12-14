5726f52cd8c398f0e2ef93ddf19f59d5c46efc3e
Manoj Reddy Pannala <manojpannala@outlook.com>
2022-12-14 17:11:58 +0200

    add api-gateway

---
+++ api-gateway/src/main/java/com/manoj/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.manoj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args){
+        org.springframework.boot.SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
